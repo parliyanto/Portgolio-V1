da5a1973f8fc183747486e15aa416e2819577cb1
Ahmad Maulana Parliyanto <79040715+parliyanto@users.noreply.github.com>
2024-10-08 04:20:56 +0700

    Update LeftBanner.js

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useTypewriter, Cursor } from "react-simple-typewriter";
-import { FaInstagram, FaLinkedinIn, FaWhatsapp, FaReact, FaBootstrap } from "react-icons/fa";
+import { FaInstagram, FaLinkedinIn, FaWhatsapp, FaReact, FaBootstrap, FaWordpress } from "react-icons/fa";
 import { SiTailwindcss, } from "react-icons/si";
+import { IoCloudDownloadOutline } from "react-icons/io5";
+import { myfile } from "../../assets/index";
 
 const LeftBanner = () => {
     const [text] = useTypewriter({
-      words: ["Website Developer.", "Front End Developer", "It Support"],
+      words: ["Website Developer.", "Front End Developer.", "It Support.", "Wordpress Developer."],
       loop: true,
-      typeSpeed: 10,
+      typeSpeed: 30,
       deleteSpeed: 10,
       delaySpeed: 2000,
     });
@@ -16,7 +18,7 @@ const LeftBanner = () => {
       <div className="flex flex-col gap-5">
         <h4 className=" text-lg font-normal">My Portfolio</h4>
         <h1 className="text-6xl font-bold text-white">
-          Hai, Saya <span className="text-designColor capitalize">Parli Yanto</span>
+          HI Me <span className="text-designColor capitalize">Parli Yanto</span>
         </h1>
         <h2 className="text-4xl font-bold text-white">
           a <span>{text}</span>
@@ -27,22 +29,21 @@ const LeftBanner = () => {
           />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wide">
-          Saya adalah lulusan sistem informasi fakultas teknik informatika saya memiliki ide berfikir yang bisa memberikan ide
-          -ide terbaru dan bidang yang saya senangi adalah membuat website<span className="text-sky-500"> Web Developer </span>
+        I am a graduate of information systems at the faculty of informatics engineering, I like working teams and I like challenges such as creating websites and also coding.
            
         </p>
       </div>
-      <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+      <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between items-center">
         <div>
-          <h2 className="text-base uppercase font-titleFont mb-4 ml-4">
-            Temukan Saya Disini 
+          <h2 className="text-base uppercase font-titleFont mb-4 ml-4 flex items-center justify-center">
+            Find Me Here 
           </h2>
           <div className="flex gap-4">
             <span className="bannerIcon">
             <a href="https://www.instagram.com/lii_parlii?igsh=MWRhaGQ3d2R0OWZ6MQ%3D%3D&utm_ssource=qr"><FaInstagram /></a>
             </span>
             <span className="bannerIcon">
-            <a href="https://wa.me/${6287767231566}?text=${encodedMessage}"><FaWhatsapp /></a>
+            <a href="https://wa.me/87767231577?text=halo saya parli silahkan lanjut ke chat untuk melanjutkan"><FaWhatsapp /></a>
             </span>
             <span className="bannerIcon">
               <a href="https://linkedin.com/in/parli-y-b194a2287"><FaLinkedinIn /></a>
@@ -51,7 +52,7 @@ const LeftBanner = () => {
         </div>
         <div>
           <h2 className="text-base uppercase font-titleFont mb-4">
-            Skill yang sedang saya kuasai saat ini
+          Skills that I am currently mastering
           </h2>
           <div className="flex justify-center items-center gap-4">
             <span className="bannerIcon2">
@@ -62,10 +63,17 @@ const LeftBanner = () => {
             </span>
             <span className="bannerIcon2">
               <SiTailwindcss />
+              </span>
+            <span className="bannerIcon2">
+              <FaWordpress />
             </span>
           </div>
         </div>
       </div>
+      <div className="flex items-center justify-center w-full p-5 hover:bg-sky-500 bg-black shadow-black font-medium uppercase rounded-md cursor-pointer">
+      <a href={myfile}>Preview My Cv</a>
+      <IoCloudDownloadOutline className="ml-5 text-lg"/>
+     </div> 
     </div>
   );
 }
