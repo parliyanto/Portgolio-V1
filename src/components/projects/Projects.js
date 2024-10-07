8668a1ce6df7e3d17222278150aba86e7fc6ac83
Ahmad Maulana Parliyanto <79040715+parliyanto@users.noreply.github.com>
2024-10-08 04:22:57 +0700

    Update Projects.js

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Title from '../layouts/Title'
-import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import { portfolioOne, projectTwo, projectThree } from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
 
 const Projects = () => {
@@ -16,42 +16,25 @@ const Projects = () => {
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
-        <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectOne}
-        />
-        <ProjectsCard
-          title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+        
+        <a href="https://www.solusimitratama.com"><ProjectsCard
+          title="Company Profile Website"
+          des="This website was created using CMS Wordpress"
+          des2=" Note : To see the project directly, please click my card"
+          src={portfolioOne}
+        /></a>
+        <a href="https://i-solution.co.id/"><ProjectsCard
+          title="Company Profile Website"
+          des="This website was created using React js, Bootstrap 5"
+          des2=" Note : To see the project directly, please click my card"
           src={projectTwo}
-        />
-        <ProjectsCard
-          title="Chatting App"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+        /></a>
+        <a href="https://e-commerce-v1-one.vercel.app/"><ProjectsCard
+          title="E-Commerce Website"
+          des="This website was creates using React JS, Tailwind UI"
+          des2=" Note : To see the project directly, please click my card"
           src={projectThree}
-        />
-        <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectThree}
-        />
-        <ProjectsCard
-          title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectOne}
-        />
-        <ProjectsCard
-          title="Chatting App"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectTwo}
-        />
+        /></a>
       </div>
     </section>
   );
